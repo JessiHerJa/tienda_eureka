d2253ed0d4d6a30bbad2e23a52b962ab18f39910
Camila088 <91265884+Camila088@users.noreply.github.com>
2021-09-27 21:30:15 -0500

    prueba
    nuevos cambios

--- tiendalostiburones.sql
+++ tiendalostiburones.sql
@@ -1,5 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.26, for macos11 (x86_64)
 Hola 
+Como estas 
 --
 -- Host: localhost    Database: tiendalostiburones
 -- ------------------------------------------------------
